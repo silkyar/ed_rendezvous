1dc5ec0885f2f0fa765507a0d0d6714b60080f8e
silkyar <silkyarora.2188@gmail.com>
2019-09-08 10:55:59 +0100

    Add edbuddy app

---
+++ edbuddy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for edbuddy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edbuddy.settings')
+
+application = get_wsgi_application()
