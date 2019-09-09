341ba2a419df22afdd6fc275fa2b82031c018fc7
silkyar <silkyarora.2188@gmail.com>
2019-09-09 19:04:25 +0100

    Add signup and login for users

--- edbuddy/settings.py
+++ edbuddy/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['local.server.com']
 INSTALLED_APPS = [
     'py_etherpadlite.apps.PyEtherpadliteConfig',
     'live_collaboration.apps.LiveCollaborationConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'edbuddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
